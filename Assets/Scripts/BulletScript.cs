0be9ad598aba2aa27358811c98f45aaa1c1eff97
Yuehua Yin <yy.yin02@gmail.com>
2023-10-18 12:24:34 +0100

    started bullet

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mouse;
+    public float speed;
+    void Start()
+    {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void Update() 
+    {
+        Debug.Log(mouse.normalized.x);
+        gameObject.velocity = new Vector2(x*speed, y*speed);
+    }
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,12 @@ public class PlayerControls : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    public float gunCooldown;
+    public float meleeCooldown;
+    public GameObject bulletPrefab;
+    public GameObject meleeAttack;
+    bool gunReady = true;
+    bool meleeReady = true;
 
     // Start is called before the first frame update
     void Start()
@@ -14,13 +20,44 @@ public class PlayerControls : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
 
-    float x = Input.GetAxis("Horizontal");
-    float y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(x*speed, y*speed);
 
-    rb.velocity = new Vector2(x*speed, y*speed);
+        // Left click to fire gun
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("fire");
+            Fire();
+        }
 
+        // Right Click to melee
+        if (Input.GetMouseButtonDown(1))
+        {
+            Melee();
+        }
+
+    }
+
+    // Fire ranged attack
+    public void Fire()
+    {
+        if (gunReady){
+            GameObject bullet = Instantiate(bulletPrefab, gameObject.transform);
+        }
+    }
+
+    // Melee attack
+    public void Melee()
+    {
+        if (meleeReady) {
+            meleeAttack.GetComponent<SpriteRenderer>().enabled = true;
+            meleeAttack.GetComponent<BoxCollider2D>().enabled = true;
+            meleeAttack.GetComponent<SpriteRenderer>().enabled = false;
+            meleeAttack.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 }
